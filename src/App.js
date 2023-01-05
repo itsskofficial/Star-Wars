9d3ac7f22a8e56a605c08312c6cfeacbc96f7849
SK <sarthakkarandikar03@gmail.com>
2023-01-05 09:24:14 +0000

    Jan 5, 2023, 2:54 PM

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
   }, [fetchMoviesHandler]);
 
   async function addMovieHandler(movie) {
-    const response =
+    const response = 
     fetch('https://star-wars-a54f2-default-rtdb.firebaseio.com/movies.json', {
       method: "POST",
       body: JSON.stringify(movie),
