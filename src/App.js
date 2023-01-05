a3023fb430f13fa1d8c5b06b07f661bd83f1e2e1
SK <sarthakkarandikar03@gmail.com>
2023-01-05 05:45:55 +0000

    Jan 5, 2023, 11:15 AM

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       const response = await fetch("https://swapi.dev/api/films")
       const data = await response.json()
       if (!response.ok) {
-        
+        throw new Error("Something went wrong")
       }
       const moviesList = data.results.map((result) => {
       return {
