45509612db3fa5ae28298d92253e1b1383704d9d
SK <sarthakkarandikar03@gmail.com>
2023-01-05 05:36:59 +0000

    Jan 5, 2023, 11:06 AM

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && <MoviesList movies={movies} />}
+        {!isLoading && movies.l<MoviesList movies={movies} />}
         {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
