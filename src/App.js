5e6bf29cb73cab592ebb14bf291f577e6438935d
SK <sarthakkarandikar03@gmail.com>
2023-01-05 08:51:28 +0000

    Jan 5, 2023, 2:21 PM

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -7,6 +7,8 @@ function App() {
   const [movies,setMovies] = useState([])
   const [isLoading, setIsLoading] = useState(false)
   const [error,setError] = useState(null)
+
+  useEffect(())
   async function fetchMoviesHandler() {
     setIsLoading(true)
     setError(null)
