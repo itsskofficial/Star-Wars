3aa7eb52ce1dede731f1075174a3bb0478917f14
SK <sarthakkarandikar03@gmail.com>
2023-01-05 05:26:51 +0000

    Jan 5, 2023, 10:56 AM

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import './App.css';
 function App() {
   const [movies,setMovies] = useState([])
   const fetchMoviesHandler = () => {
-    
-    fetch("https://swapi.dev/api/films").then((response) => {
+    const response=await 
+   .then((response) => {
       return response.json()
     }).then((data) => {
       const moviesList = data.results.map((result) => {
